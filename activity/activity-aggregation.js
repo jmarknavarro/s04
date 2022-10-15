4627784811ac2bb8914fb691c5bbb8c804d8ac0d
John Mark Navarro <90978126+jmarknavarro@users.noreply.github.com>
2022-10-15 20:30:37 +0800

    aggregation activity added

---
+++ activity/activity-aggregation.js
@@ -0,0 +1,97 @@
+let employee = [{
+    
+    _id: "312456",
+    EmployeeName : "Keneth Meier",
+    EmployeeAge : "28",
+    jobRole: "Store Manager",
+    Salary : "65000"
+},
+
+{
+    _id : "345342",
+    EmployeeName : "Jemiah Kim",
+    EmployeeAge : "26",
+    jobRole : "Store Associate",
+    Salary : "35000"
+},
+
+{
+    _id : "334566",
+    EmployeeName : "Ralph Mariano",
+    EmployeeAge : "35",
+    jobRole : "Cashier",
+    Salary : "25000"
+}];
+
+let inventory = [
+
+{
+    _id : "LSOOOO123",
+    Name: "XYZ - 100g",
+    Price : "5.23",
+    Quantity : "25000",
+    Category : "chocolate, sweets"
+},
+
+{
+    _id : "LS0003123",
+    Name : "XY - 1L",
+    Price : "3",
+    Quantity : "1000",
+},
+
+{
+    _id : "LS0002688",
+    Name : "XCY - 500g",
+    Price : "25",
+    Quantity : "500",
+}];
+
+let payment = [
+
+{
+    _id : "BL20215",
+    grossAmount : "105.65",
+    Discounts : "15.5",
+    netAmount : "95.65",
+    DateTime : "ISODate(2021-01-01T16:00:00Z)"
+},
+
+{
+    _id : "BL20216",
+    grossAmount : "65.55",
+    Discounts : "0",
+    netAmount : "45.25",
+    DateTime : "ISODate(2021-01-01T16:15:55Z)"
+},
+
+{
+    _id : "BL20217",
+    grossAmount : "33.5",
+    Discounts : "17.5",
+    netAmount : "13",
+    DateTime : "(2021-01-01T11:02:35Z)"
+}];
+
+let promo = [
+
+{
+    _id : "PROMO01",
+    Name : "3 Days Sale Promo",
+    Period : "7",
+    dailySales : "45, 15, 60"
+},
+
+{
+    _id : "PROMO02",
+    Name : "11.11 Promo",
+    Period : "2",
+    dailySales : "120, 200"
+},
+
+{
+    _id : "PROMO04",
+    Name : "New Year Promo",
+    Period : "7",
+    dailySales : "74, 278, 350"
+}	];	
